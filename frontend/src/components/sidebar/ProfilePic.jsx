b4be73e1a29d57271c61066ab43c79bcfbd95a25
yogesh verma <98902068+yogi-verma@users.noreply.github.com>
2024-08-01 22:24:38 +0530

    ui update

--- frontend/src/components/sidebar/ProfilePic.jsx
+++ frontend/src/components/sidebar/ProfilePic.jsx
@@ -5,7 +5,7 @@ const Profile = () => {
     return (
         <div className='flex flex-col sm:flex-row items-center bg-blue-600 shadow-2xl border-none mb-3 p-3 rounded-2xl hover:shadow-lg cursor-pointer transform hover:scale-95 transition-all duration-300'>
             <div className='relative mb-3 flex-shrink-0'>
-                <div className='w-12 h-12 md:w-16 md:h-16 rounded-full overflow-hidden'>
+                <div className='w-9 md:w-9 md:h-9 rounded-full overflow-hidden'>
                     <img src={authUser.profilePic} alt='user avatar' className='object-cover w-full h-full' />
                 </div>
                 <span className='absolute bottom-0 right-0 block w-4 h-4 md:w-5 md:h-5 bg-green-500 border-2 border-white rounded-full'></span>
